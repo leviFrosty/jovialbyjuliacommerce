084b1c57c80e8555ea812b68622cca75f4a7b1f5
Levi Wilkerson <chelleas123@gmail.com>
2021-04-21 12:32:10 -0400

    Added hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Test from "./components/test";
 import NavBar from "./components/navBar";
+import Hero from "./components/hero";
 
 const App = () => {
   return (
     <React.Fragment>
       <NavBar />
+      <Hero />
       <Test />
     </React.Fragment>
   );
