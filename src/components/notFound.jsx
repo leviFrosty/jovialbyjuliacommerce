b5eb9fd70d469828d8e8f63f07184b4f5f1a32cc
Levi Wilkerson <chelleas123@gmail.com>
2021-04-29 15:30:05 -0400

    update notfound page text

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -6,7 +6,10 @@ const NotFound = () => {
   return (
     <div className="container d-flex justify-content-center flex-column align-items-center mb-5 mt-5 vh-100 position-relative">
       <div>
-        <h1 className="text-center">Oops, lost?</h1>
+        <h1 className="text-center">Error 404</h1>
+        <p className="text-center">
+          You look lost. Here are some suggested links:
+        </p>
       </div>
       <div>
         <Link className="btn btn-primary m-2" to="/home">
@@ -15,8 +18,8 @@ const NotFound = () => {
         <Link className="btn btn-primary m-2" to="/products">
           Products
         </Link>
-        <Link className="btn btn-primary m-2" to="/home">
-          Home
+        <Link className="btn btn-primary m-2" to="/contact">
+          Contact
         </Link>
       </div>
       <Lost className="lost-image" />
